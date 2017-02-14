c303b61f41153055755731674adc44ccb4b2e07d
Amit Chaudhari <amitchaudhari@Amits-MacBook.local>
2017-02-14 17:01:50 -0500

    initial push to heroku take 2

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import {Provider } from 'react-redux';
 import {Router, Route, IndexRoute, hashHistory } from 'react-router';
-import App from './App';
+import App from './app';
 
 const Root = (props) =>{
   return(
     <Provider store={props.store}>
       <Router history={hashHistory}>
         <Route path='/' component={App}>
-          
+
         </Route>
       </Router>
     </Provider>
