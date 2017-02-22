4ed13b9869bf58653db936652523a0875d667dfa
Amit Chaudhari <amitchaudhari@Amits-MacBook.local>
2017-02-22 01:16:26 -0500

    more heroku fixes

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module Rubygogo
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
     # config.serve_static_assets = true
-    config.assets.compile = true
+    # config.assets.compile = true
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
