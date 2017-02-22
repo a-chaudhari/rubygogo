c8dd8eda42909eca60f045f275f9c26a6a5b6fd9
Amit Chaudhari <amitchaudhari@Amits-MacBook.local>
2017-02-22 00:56:25 -0500

    styled the start campaign page

--- frontend/components/create.jsx
+++ frontend/components/create.jsx
@@ -5,7 +5,7 @@ class Create extends React.Component{
     super(props);
     this.state={
       goal_amount: 500,
-      title: "test",
+      title: "",
       currency: "USD"
     };
     this.update=this.update.bind(this);
@@ -23,11 +23,16 @@ class Create extends React.Component{
   render(){
     return(
       <div className="start-campaign-lobby">
-        <h1>Start a campaign</h1>
+        <h1 className="start-campaign-title">Start a campaign</h1>
+        <h3 className="start-campaign-subtitle">Raise funds for creative, entrepreneurial, or other passion projects.</h3>
         <form onSubmit={this.submitForm.bind(this)}>
-          <input placeholder="amount seeking" value={this.state.goal_amount} onChange={this.update('goal_amount')}/>
-          <input placeholder="title" value={this.state.title} onChange={this.update('title')}/>
-          <button>Submit</button>
+          <label>How much money would you like to raise?
+            <input value={this.state.goal_amount} onChange={this.update('goal_amount')}/>
+          </label>
+          <label>What is the title of your Campaign?
+            <input placeholder="My campaign title..." value={this.state.title} onChange={this.update('title')}/>
+          </label>
+          <button className="newcampaign-create-button">Create My Campaign</button>
         </form>
       </div>
     );
